8eda944a2d100743368dd20cfde8f82a0770a7cb
Paulo Bandeira <paulounisul@gmail.com>
2021-05-25 08:32:44 -0300

    Finalizado aula de transação 2, rodando perfeito

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final f = NumberFormat('#,###.0#', 'pt_BR');
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tr) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  //interpolação de valores
+                  //'R\$ ${tr.value.toStringAsFixed(2)}',
+                  //usado a formatacao que esta na intl..
+                  'R\$ ${f.format(tr.value)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tr.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(tr.date),
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
